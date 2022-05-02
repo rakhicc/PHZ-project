42ca5edcb6a81e3323063a77c54ef8f37e335d2f
MariaRosenholm <maria.rosenholm@gmail.com>
2022-05-02 15:39:25 +0300

    css fixes

--- src/pages/AfterSubmit.js
+++ src/pages/AfterSubmit.js
@@ -1,15 +1,17 @@
 import React from "react";
 import Button from "../UI/Button";
-import styles from "./OverLay.module.css";
+import styles from "./AfterSubmit.module.css";
 
 const AfterSubmit = ({ message }) => {
   return (
-    <div data-testid="AfterSubmit" className={styles.popup}>
-      <div className={styles.allContent}>
-        <h2>{message}</h2>
-        <a href="/">
-          <Button>Close</Button>
-        </a>
+    <div className={styles.overlay}>
+      <div data-testid="AfterSubmit" className={styles.popup}>
+        <div className={styles.allContent}>
+          <h2>{message}</h2>
+          <a href="/">
+            <Button>Close</Button>
+          </a>
+        </div>
       </div>
     </div>
   );
